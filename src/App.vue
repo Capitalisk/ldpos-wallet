85b1a54a7f7816ec35005c099b661f9d56d8d658
Maarten Coppens <martinuske@gmail.com>
2021-03-04 12:11:08 -0300

    added dark-theme

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,49 @@
 <template>
+  <button @click="toggleDarkMode">Toggle Darkmode</button>
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+  name: 'App',
+  setup() {
+    const darkMode = ref(
+      window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches,
+    );
+
+    const toggleDarkMode = () => {
+      console.log('hit');
+      darkMode.value = !darkMode.value;
+      console.log(darkMode)
+    };
+
+    watch(
+      () => darkMode.value,
+      () => {
+        console.log(darkMode.value)
+        document.documentElement.setAttribute('dark-theme', darkMode.value)
+      }
+    );
+
+    return {
+      darkMode,
+      toggleDarkMode,
+    };
+  },
+};
+</script>
+
 <style>
+@import url('./styles/main.css');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
