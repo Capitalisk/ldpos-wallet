31468a99198bc948956db1068254496eb6fba1d4
Maarten Coppens <martinuske@gmail.com>
2021-03-16 11:51:01 -0300

    Split Sidebar

---
+++ src/components/sections/Sidebar.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="sidebar">
+    <a to="/" class="first" @click="toggleModal(TOKENMODAL)">CLSK</a>
+    <hr />
+    <router-link to="/"><i class="fa fa-home mr-1" />DASHBOARD </router-link>
+    <a @click="toWallet" :class="authenticated ? '' : 'disabled'">
+      <i class="fa fa-wallet mr-1" />WALLET
+    </a>
+    <router-link to="/delegates">
+      <i class="fa fa-poll mr-1" />VOTING
+    </router-link>
+    <hr />
+    <router-link to="/">
+      <i class="fa fa-exchange-alt mr-1" />TRANSACTIONS
+    </router-link>
+    <router-link to="/"> <i class="fa fa-th-large mr-1" />BLOCKS </router-link>
+    <router-link to="/"> <i class="fa fa-users mr-1" />ACCOUNTS </router-link>
+    <router-link to="/">
+      <i class="fa fa-users-cog mr-1" />DELEGATES
+    </router-link>
+    <hr />
+    <router-link to="/">
+      <i class="fa fa-signature mr-1" />SIGN MESSAGE
+    </router-link>
+    <router-link to="/" class="last">
+      <i class="fa fa-check mr-1" />VERIFY MESSAGE
+    </router-link>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+  setup() {
+    const store = useStore()
+
+    return {
+      authenticated: computed(() => store.state.authenticated)
+    }
+  },
+}
+</script>
+
+<style scoped>
+.sidebar {
+  position: fixed;
+  width: 250px;
+  padding: 0;
+  margin: 1rem;
+  border-radius: var(--border-radius);
+  /* background: linear-gradient(-30deg, var(--primary), var(--primary-lightest)); */
+  background-color: var(--primary);
+  color: var(--white);
+  text-transform: uppercase;
+}
+
+.sidebar hr {
+  border: 0;
+  border-top: 1px solid rgba(12, 138, 228, 0.4);
+  border-width: 1px;
+}
+
+.sidebar a {
+  color: var(--dark);
+  text-decoration: none;
+  padding: 1rem;
+  display: block;
+  font-size: 12px;
+  cursor: pointer;
+}
+
+a.disabled {
+  color: var(--disabled);
+  cursor: default;
+  user-select: none;
+}
+
+a.disabled:hover {
+  background-color: initial !important;
+}
+
+.sidebar a:hover {
+  background-color: var(--primary-lightest);
+}
+
+.first {
+  border-top-left-radius: var(--border-radius);
+  border-top-right-radius: var(--border-radius);
+}
+
+.last {
+  border-bottom-left-radius: var(--border-radius);
+  border-bottom-right-radius: var(--border-radius);
+}
+</style>
