979877803afb104419792ffbbbf8c0b6ec09f03d
Maarten Coppens <martinuske@gmail.com>
2021-05-12 11:15:32 -0300

    format

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
-export const _decimalToInteger = (amount) =>
+export const _decimalToInteger = amount =>
   amount !== '' && (parseFloat(amount) * 100000000).toString();
 
-export const _integerToDecimal = (amount) => {
+export const _integerToDecimal = amount => {
   const parsedAmount = parseInt(amount);
   const calculatedAmount = parsedAmount / 100000000;
   const isInt = Number.isInteger(parsedAmount);
@@ -14,7 +14,7 @@ export const _integerToDecimal = (amount) => {
     : '';
 };
 
-export const _parseDate = (d) => {
+export const _parseDate = d => {
   if (!d) return;
   d = new Date(d);
   const today = new Date();
@@ -36,10 +36,10 @@ export const _parseDate = (d) => {
 export const _transformMonetaryUnit = (amount, symbol = 'CLSK') =>
   amount ? `${_integerToDecimal(amount)} ${symbol.toUpperCase()}` : null;
 
-export const _splitCamelCaseWords = (str) => {
+export const _splitCamelCaseWords = str => {
   if (typeof str !== 'string') return [];
   return str.replace(/([A-Z]+|[A-Z]?[a-z]+)(?=[A-Z]|\b)/g, '!$&').split('!');
 };
 
-export const _capitalize = (str) =>
-  str.replace(/(?:^|\s|[-"'([{])+\S/g, (c) => c.toUpperCase());
+export const _capitalize = str =>
+  str.replace(/(?:^|\s|[-"'([{])+\S/g, c => c.toUpperCase());
