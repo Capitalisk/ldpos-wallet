ab86820eb2edba334f47b58d50606e1fb5856417
Maarten Coppens <martinuske@gmail.com>
2021-05-21 08:25:14 -0300

    rename Settings #49

--- src/router/index.js
+++ src/router/index.js
@@ -43,10 +43,10 @@ const routes = [
       import(/* webpackChunkName: "accounts" */ '../views/Accounts.vue'),
   },
   {
-    path: '/settings',
-    name: 'Settings',
+    path: '/blockchains',
+    name: 'Blockchains',
     component: () =>
-      import(/* webpackChunkName: "settings" */ '../views/Settings.vue'),
+      import(/* webpackChunkName: "blockchains" */ '../views/Blockchains.vue'),
   },
 ];
 
