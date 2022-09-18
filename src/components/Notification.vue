d8f0708abd7c3c95450843dcf98e9433c4e7d60a
Maarten Coppens <martinuske@gmail.com>
2022-09-18 17:51:51 -0300

    significantly improve notification with transitions and time indicator improve `Progressbar` component

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,26 +1,34 @@
 <template>
-  <template v-if="notifications">
-    <div
-      v-for="(notification, i) in notifications"
-      :key="notification"
-      class="notification pa-4 ma-2"
-      :class="notification.error ? 'danger' : ''"
-      :ref="
-        el => {
-          if (el) divs[i] = el;
-        }
-      "
-    >
-      <div class="close-btn cursor-pointer" @click="denotify(i)">
-        &#10005;
+  <div class="notifications" v-if="notifications">
+    <transition-group name="list">
+      <div
+        v-for="(notification, i) in Object.values(notifications).reverse()"
+        :key="notification"
+        class="notification pa-2 ma-2"
+        :class="notification.error ? 'danger' : ''"
+        :ref="
+          (el) => {
+            if (el) divs[i] = el;
+          }
+        "
+      >
+        <Progressbar
+          active
+          class="notification-progress"
+          :value="`${(notification.secondsLeft / notification.seconds) * 100}%`"
+        />
+        <div class="close-btn cursor-pointer" @click="denotify(i)">
+          &#10005;
+        </div>
+        <div class="wrap mr-2">{{ notification.message }}</div>
       </div>
-      <div class="wrap mr-2">{{ notification.message }}</div>
-    </div>
-  </template>
+    </transition-group>
+  </div>
 </template>
 
 <script setup>
-import { inject, onBeforeUpdate, onUpdated, ref } from 'vue';
+import { computed, inject, onBeforeUpdate, onUpdated, ref } from 'vue';
+import Progressbar from './Progressbar.vue';
 
 const store = inject('store');
 const divs = ref([]);
@@ -35,21 +43,24 @@ onUpdated(() => {
   }
 });
 
-const notifications = store.state.notifications;
+const notifications = computed(() => store.state.notifications);
 
-const denotify = i => store.denotify(i);
+const denotify = (i) => store.denotify(i);
 </script>
 
 <style scoped>
-.notification {
+.notifications {
   position: fixed;
-  border: 1px solid var(--dark);
-  border-radius: var(--border-radius);
   bottom: 0px;
   right: 0px;
+}
+
+.notification {
+  border-radius: var(--border-radius);
   background-color: var(--primary);
   max-width: 30vw;
   z-index: 10;
+  backdrop-filter: blur(4px);
 }
 
 .notification.danger {
@@ -60,8 +71,26 @@ const denotify = i => store.denotify(i);
 
 .close-btn {
   position: absolute;
-  right: var(--unit-2);
+  right: var(--unit-1);
+  top: var(--unit-1);
   font-size: var(--unit-2);
   color: var(--dark) !important;
+}
+
+.notification-progress {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
